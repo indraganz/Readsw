7e486adf3872428ac21e58e3585239621cfb6cf5
Indraxyz <141424803+indraganz@users.noreply.github.com>
2025-02-20 10:48:27 +0700

    Add files via upload

---
+++ plugins/setting.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+export default {
+   name: ['anticall', 'readsw', 'reactsw', 'readchat', 'autotyping', 'self', 'online'],
+   command: ['anticall', 'readsw', 'reactsw', 'readchat', 'autotyping', 'self', 'online'],
+   tags: 'setting',
+   run: async (m, { sock, text, command, setting }) => {
+      if (!text) {
+         let allStatus = Object.keys(setting)
+            .filter(key => typeof setting[key] === 'boolean')
+            .map(key => `- ${key} = ${setting[key] ? 'ON' : 'OFF'}`)
+            .join('\n');
+         return sock.reply(m.from, `Masukkan ON/OFF\n\nCurrent all status:\n${allStatus}`, m);
+      }
+
+      if (!['on', 'off'].includes(text.toLowerCase())) {
+         return sock.reply(m.from, `Masukkan ON/OFF`, m);
+      }
+
+      setting[command] = text.toLowerCase() === 'on';
+      const path = './setting.js';
+      const newContent = `export default ${JSON.stringify(setting, null, 3)}\n`;
+      fs.writeFileSync(path, newContent, 'utf-8');
+
+      sock.reply(m.from, `Berhasil ${text.toLowerCase() === 'on' ? 'Mengaktifkan' : 'Menonaktifkan'} ${command}`, m);
+   },
+   owner: true
+};
