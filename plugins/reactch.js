228a31b037bd470404db89426dc739301333c981
Indraxyz <141424803+indraganz@users.noreply.github.com>
2025-05-10 15:59:59 +0700

    Update reactch.js

--- plugins/reactch.js
+++ plugins/reactch.js
@@ -1,38 +1,36 @@
 export default {
-  name: ['reactch'],
+  name: 'reactch',
   command: ['reactch'],
   tags: 'tool',
-  owner: false,
   run: async (m, { sock, text }) => {
+    if (!text) {
+      return sock.sendMessage(m.from, { text: 'Masukkan link channel WhatsApp dengan format yang benar.' }, { quoted: m });
+    }
+
+    const match = text.match(/https:\/\/whatsapp\.com\/channel\/(\w+)(?:\/(\d+))?/);
+    if (!match) {
+      return sock.sendMessage(m.from, { text: 'URL tidak valid. Silakan periksa kembali.' }, { quoted: m });
+    }
+
+    const channelId = match[1];
+    const chatId = match[2];
+    if (!chatId) {
+      return sock.sendMessage(m.from, { text: 'ID chat tidak ditemukan dalam link yang diberikan.' }, { quoted: m });
+    }
+
     try {
-      if (!text) {
-        return sock.reply(m.from, 'Masukkan link channel WhatsApp dengan format yang benar.', m);
-      }
-
-      const match = text.match(/https:\/\/whatsapp\.com\/channel\/(\w+)(?:\/(\d+))?/);
-      if (!match) {
-        return sock.reply(m.from, 'URL tidak valid. Silakan periksa kembali.', m);
-      }
-
-      const channelId = match[1];
-      const messageId = match[2];
-      const reaction = text.split(" ").slice(1).join(" ") || '❤️';
-
-      if (!messageId) {
-        return sock.reply(m.from, 'ID chat tidak ditemukan dalam link yang diberikan.', m);
-      }
-
       const data = await sock.newsletterMetadata("invite", channelId);
       if (!data) {
-        return sock.reply(m.from, 'Newsletter tidak ditemukan atau terjadi kesalahan.', m);
+        return sock.sendMessage(m.from, { text: 'Newsletter tidak ditemukan atau terjadi kesalahan.' }, { quoted: m });
       }
 
-      await sock.newsletterReactMessage(data.id, messageId, reaction);
-      sock.reply(m.from, 'Sukses mengirim reaction ke pesan di channel.', m);
+      await sock.newsletterReactMessage(data.id, chatId, text.split(" ").slice(1).join(" ") || '❤️');
+      return sock.sendMessage(m.from, { text: 'Sukses mengirim reaction ke pesan di channel.' }, { quoted: m });
 
     } catch (e) {
       console.error(e);
-      sock.reply(m.from, `Terjadi kesalahan:\n${e.message}`, m);
+      return sock.sendMessage(m.from, { text: `Terjadi kesalahan:\n${e.message}` }, { quoted: m });
     }
-  }
+  },
+  location: __filename
 };
