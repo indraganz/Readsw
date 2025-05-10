b4b20cb9939aac198aecf91b2bd8b2f7c73547a3
Indraxyz <141424803+indraganz@users.noreply.github.com>
2025-05-10 15:50:56 +0700

    Update rvo.js

--- plugins/rvo.js
+++ plugins/rvo.js
@@ -1,11 +1,21 @@
 export default {
-   name: ['rvo'],
-   command: ['rvo', 'readviewonce'],
-   tags: 'main',
-   run: async (m, { sock, q }) => {
-      if (!q.msg.viewOnce) return sock.reply(m.from, 'Reply to a message with .rvo', m)
-        q.msg.viewOnce = false
-        await m.reply({ forward: q, force: true })
-   },
-   owner: false
-}
+  name: ['rvo'],
+  command: ['rvo', 'readviewonce', 'read', 'liat'],
+  tags: 'main',
+  owner: false,
+  run: async (m, { sock, q }) => {
+    try {
+      if (!q || !q.msg || !q.msg.viewOnce) {
+        return sock.reply(m.from, '🚩 Reply view once media to use this command.', m);
+      }
+
+      // Buka viewOnce dan forward media
+      q.msg.viewOnce = false;
+      await sock.relayMessage(m.from, q, { messageId: m.id, force: true });
+
+    } catch (e) {
+      console.error(e);
+      sock.reply(m.from, 'Terjadi kesalahan:\n' + e.message, m);
+    }
+  }
+};
